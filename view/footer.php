efddff999476d6e251943083cea83b29d907e532
Ishver Nayak <ishver@dswtpl.com>
2017-02-27 11:20:46 +0530

    dones

--- view/footer.php
+++ view/footer.php
@@ -112,15 +112,15 @@
             <input value="Submit" class="apply-btn" id="subspartner" type="partner">
     </form>
 </div>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/fancySelect.js"></script>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/custom.js"></script>
-<script type="text/javascript" src="<?php echo $baseURL; ?>js/fancybox.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/jquery.min.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/bootstrap.min.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/owl.carousel.min.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/fancySelect.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/custom.js"></script>
+<script async type="text/javascript" src="<?php echo $baseURL; ?>js/fancybox.js"></script>
 <?php if($p=='partner-activity-log-page' || $p=='partner-activity-log'){?>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+    <script async type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script async type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
 
     <script type="text/javascript">
 $(document).ready(function() {
