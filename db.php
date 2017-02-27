f21b103021feb70aef098f953ee230f293dc7831
Ishver Nayak <ishver@dswtpl.com>
2017-02-27 16:05:02 +0530

    dones

--- db.php
+++ db.php
@@ -1,25 +1,10 @@
 <?php
 
-if($_SERVER['HTTP_HOST']=='192.168.1.24')
-{
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "norlinx";
-$baseURL="http://192.168.1.24/norlinx/";
-} else if($_SERVER['HTTP_HOST']=='45.55.179.114'){
 $servername = "localhost";
 $username = "root";
 $password = "ishver@123";
 $dbname = "norlinx";
 $baseURL="http://45.55.179.114/norlinx/";
-} else {
-$servername = "localhost";
-$username = "root";
-$password = "wazir@password123";
-$dbname = "norlinx";
-$baseURL="http://norlinxhtml.wazirapp.com/";
-}
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $partnerSliderText="Customer Sample:";
